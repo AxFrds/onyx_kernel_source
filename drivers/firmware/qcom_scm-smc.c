6aae457090819b4e6561c0492bd39caa43607184
AxFrds <afrds.x@gmail.com>
2026-07-01 15:40:09 +0000

    firmware: qcom: update SCM helper interfaces

--- drivers/firmware/qcom_scm-smc.c
+++ drivers/firmware/qcom_scm-smc.c
@@ -13,7 +13,6 @@
 #include <linux/arm-smccc.h>
 #include <linux/dma-mapping.h>
 #include <linux/qtee_shmbridge.h>
-#include <linux/wait.h>
 
 #include "qcom_scm.h"
 
@@ -24,10 +23,6 @@
 struct arm_smccc_args {
 	unsigned long args[8];
 };
-
-//Mi-Security Add
-DECLARE_WAIT_QUEUE_HEAD(tzdbg_log_wq);
-EXPORT_SYMBOL(tzdbg_log_wq);
 
 static DEFINE_MUTEX(qcom_scm_lock);
 
@@ -302,7 +297,7 @@ int __scm_smc_call(struct device *dev, const struct qcom_scm_desc *desc,
 	}
 
 	ret = (long)smc_res.a0 ? qcom_scm_remap_error(smc_res.a0) : 0;
-	wake_up_interruptible(&tzdbg_log_wq);
+
 	return ret;
 
 }
